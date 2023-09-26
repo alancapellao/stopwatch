9ef949832ea93894078dc3b51f962d930a363ab1
alancptk7 <alancptk.7@gmail.com>
2023-09-26 15:15:10 -0300

    corrections script

--- scripts/script.js
+++ scripts/script.js
@@ -1,78 +1,50 @@
-"use strict"
-
-var hh = 0;
-var mm = 0;
-var ss = 0;
-var mi = 0;
-
-var tempo = 10;
+var hh = 0, mm = 0, ss = 0, mi = 0, i = 0;
 var cron;
+const temp = document.getElementById('temp');
 
-var button = document.querySelector('.btn');
-
-var i = 0;
-
-button.addEventListener('click', function click(e) {
+document.querySelector('.btn').addEventListener('click', function click(e) {
     i++;
 });
 
 function start() {
-
-
     if (i == 0) {
-        cron = setInterval(() => { timer(); }, tempo);
+        cron = setInterval(() => { timer(); }, 10);
     }
 }
 
 function stop() {
-
     clearInterval(cron);
     i = 0;
 }
 
 function reset() {
-
     clearInterval(cron);
-    hh = 0;
-    mm = 0;
-    ss = 0;
-    mi = 0;
-    i = 0;
-
-    document.getElementById('temp').innerHTML = "00:00";
+    temp.textContent = '00:00';
+    hh = 0, mm = 0, ss = 0, mi = 0, i = 0;
 }
 
 function timer() {
-
     mi++;
 
-    if (mi == 100) {
+    ss += Math.floor(mi / 100);
+    mi %= 100;
 
-        mi = 0;
-        ss++;
+    mm += Math.floor(ss / 60);
+    ss %= 60;
 
-        if (ss == 60) {
-            ss = 0;
-            mm++;
+    hh += Math.floor(mm / 60);
+    mm %= 60;
 
-            if (mm == 60) {
-                mm = 0;
-                hh++;
-            }
-        }
+    function formatNumber(num) {
+        return num < 10 ? '0' + num : num.toString();
     }
-    var format = (ss < 10 ? '0' + ss : ss) + ':' + (mi < 10 ? '0' + mi : mi);
-    document.getElementById('temp').innerText = format;
 
-    if (mm > 0) {
+    var parts = [];
+    if (hh > 0) parts.push(formatNumber(hh));
+    if (mm > 0 || hh > 0) parts.push(formatNumber(mm));
+    parts.push(formatNumber(ss));
+    parts.push(formatNumber(mi));
 
-        format = (mm < 10 ? '0' + mm : mm) + ':' + (ss < 10 ? '0' + ss : ss) + ':' + (mi < 10 ? '0' + mi : mi);
-        document.getElementById('temp').innerText = format;
-
-        if (hh > 0) {
-
-            format = (hh < 10 ? '0' + hh : hh) + ':' + (mm < 10 ? '0' + mm : mm) + ':' + (ss < 10 ? '0' + ss : ss) + ':' + (mi < 10 ? '0' + mi : mi);
-            document.getElementById('temp').innerText = format;
-        }
-    }
+    var format = parts.join(':');
+    temp.textContent = format;
 }
